d07bb6d8753d1f493b9abfc92bb25d83e65bd769
Frankie-Devito <60734988+Frankie-Devito@users.noreply.github.com>
2020-02-07 04:27:14 -0800

    blah

--- home.js
+++ home.js
@@ -6,3 +6,11 @@ $(function(){
 	$('#post').fadeTo(3000, 1);
 })
 });
+// Home Page Pulsing Image
+$(function(){
+	$('#p1').hide().fadeTo(3000, 1);
+	setInterval(function(){
+	$('#p1').fadeTo(3000, 0.2);
+	$('#p1').fadeTo(3000, 1);
+})
+});
